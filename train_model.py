fe847aa8ee39c3313524af48925493ecd95b2030
Erik Hamberg <erikhamb@kth.se>
2024-04-10 10:42:48 +0200

    added device number to select gpu

--- train_model.py
+++ train_model.py
@@ -30,53 +30,53 @@ def train_model(training_params):
     except KeyError:
         pass
 
-    good_runs_string = """20240405-100841_Process-2
-20240405-100841_Process-4
-20240405-175545_Process-2
-20240405-175545_Process-3
-20240405-175545_Process-4
-20240405-175545_Process-5
-20240406-002836_Process-3"""
-
-    training_param_string = """20240405-100841_Process-2
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.01, L2_LAMBDA: 0.01
-20240405-100841_Process-4
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.01, L2_LAMBDA: 0.1
-20240405-175545_Process-2
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.1, L2_LAMBDA: 0.1
-20240405-175545_Process-3
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 0.01
-20240405-175545_Process-4
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.1, L2_LAMBDA: 1
-20240405-175545_Process-5
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 0.1
-20240406-002836_Process-3
-LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 1
-"""
-
-    # Make dictionary of good run params
-    good_runs = {}
-    first = True
-    val = ""
-    for run in training_param_string.split("\n"):
-        if first:
-            val = run
-            first = False
-        else:
-            first = True
-            good_runs[run] = val
-
-    # Get the key for this run
-    key = f"LEARNING_RATE: {LEARNING_RATE}, TILE_SIZE: {TILE_SIZE}, DEPTH_PADDING: {DEPTH_PADDING}, MIN_ENCODER_DIM: {MIN_ENCODER_DIM}, EPOCHS: {9}, TRUE_BATCH_SIZE: {TRUE_BATCH_SIZE}, PIC_BATCH_SIZE: {PIC_BATCH_SIZE}, SAVE_MODEL: {SAVE_MODEL}, L1_LAMBDA: {L1_LAMBDA}, L2_LAMBDA: {L2_LAMBDA}"
-
-    model_dir = None
-    try:
-        model_dir = good_runs[key]
-        print(f"Model found: {model_dir}")
-        print(f"Using params {key}")
-    except KeyError:
-        print("Key not found")
-        return
+#     good_runs_string = """20240405-100841_Process-2
+# 20240405-100841_Process-4
+# 20240405-175545_Process-2
+# 20240405-175545_Process-3
+# 20240405-175545_Process-4
+# 20240405-175545_Process-5
+# 20240406-002836_Process-3"""
+#
+#     training_param_string = """20240405-100841_Process-2
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.01, L2_LAMBDA: 0.01
+# 20240405-100841_Process-4
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.01, L2_LAMBDA: 0.1
+# 20240405-175545_Process-2
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.1, L2_LAMBDA: 0.1
+# 20240405-175545_Process-3
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 0.01
+# 20240405-175545_Process-4
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 0.1, L2_LAMBDA: 1
+# 20240405-175545_Process-5
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 0.1
+# 20240406-002836_Process-3
+# LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPOCHS: 9, TRUE_BATCH_SIZE: 24, PIC_BATCH_SIZE: 3, SAVE_MODEL: True, L1_LAMBDA: 1, L2_LAMBDA: 1
+# """
+#
+#     # Make dictionary of good run params
+#     good_runs = {}
+#     first = True
+#     val = ""
+#     for run in training_param_string.split("\n"):
+#         if first:
+#             val = run
+#             first = False
+#         else:
+#             first = True
+#             good_runs[run] = val
+#
+#     # Get the key for this run
+#     key = f"LEARNING_RATE: {LEARNING_RATE}, TILE_SIZE: {TILE_SIZE}, DEPTH_PADDING: {DEPTH_PADDING}, MIN_ENCODER_DIM: {MIN_ENCODER_DIM}, EPOCHS: {9}, TRUE_BATCH_SIZE: {TRUE_BATCH_SIZE}, PIC_BATCH_SIZE: {PIC_BATCH_SIZE}, SAVE_MODEL: {SAVE_MODEL}, L1_LAMBDA: {L1_LAMBDA}, L2_LAMBDA: {L2_LAMBDA}"
+#
+#     model_dir = None
+#     try:
+#         model_dir = good_runs[key]
+#         print(f"Model found: {model_dir}")
+#         print(f"Using params {key}")
+#     except KeyError:
+#         print("Key not found")
+#         return
 
     if loader == False:
         dataset = training_params.get('dataset', None)
@@ -119,7 +119,7 @@ LEARNING_RATE: 0.001, TILE_SIZE: 128, DEPTH_PADDING: 2, MIN_ENCODER_DIM: 16, EPO
     process = multiprocessing.current_process().name
 
     time_stamp = time.strftime("%Y%m%d-%H%M%S")
-    log_dir = f"runs/{model_dir}"
+    log_dir = f"runs/{time_stamp}_{process}"
 
     fake_writer = SummaryWriter(f"{log_dir}/fake")
     real_writer = SummaryWriter(f"{log_dir}/real")
