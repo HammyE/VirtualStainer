9b61bcee09f76bfead425200c9e8f2a6615cd4c2
Erik Hamberg <erikhamb@kth.se>
2024-05-09 21:32:39 +0200

    Changed training parameters to push for more L1 heavy models

--- grid_search_run.py
+++ grid_search_run.py
@@ -14,6 +14,7 @@ from train_model import train_model
 
 data_dir = 'dataset'
 
+
 def worker_func(shared_param_sets, lock, gpu_id):
     torch.cuda.set_device(gpu_id)  # Set GPU for this process
     while True:
@@ -27,7 +28,6 @@ def worker_func(shared_param_sets, lock, gpu_id):
         # Now, train the model with these parameters on the assigned GPU
         params['DEVICE'] = torch.device(f'cuda:{gpu_id}')
         train_model(params)  # Your training function
-
 
 
 if __name__ == '__main__':
@@ -49,7 +49,6 @@ if __name__ == '__main__':
     SAVE_MODEL = True
     DEPTH_PADDING = 2
 
-
     # For M1 Macs check for mps
     if DEVICE.type == 'cpu':
         if torch.backends.mps.is_available():
@@ -66,7 +65,6 @@ if __name__ == '__main__':
 
     DEBUG = False
 
-
     # load data
     transform = transforms.Compose([
         RangeTransform(in_range=(0, 255), out_range=(0, 1)),
@@ -74,42 +72,48 @@ if __name__ == '__main__':
 
     print("Loading dataset...")
 
-
-
     print("Dataset loaded.")
 
     print("Loading data loader...")
 
     print("Data loader loaded.")
 
-
     learning_rate = [0.01]
     l1_lambda = [0.1, 0.5]
     l2_lambda = [0.1]
-    D_LR = [0.01]#[0.001, 0.005, 0.01]
+    D_LR = [0.01]  # [0.001, 0.005, 0.01]
     G_LR = [0.001, 0.01]
-    run_names = [#"20240422-205546_Process-4",
-"20240423-165651_Process-3",
-"20240423-165649_Process-5",
-"20240423-165127_Process-4",
-"20240423-165122_Process-2",
-"20240423-115702_Process-5",
-"20240423-115653_Process-3",
-"20240423-115255_Process-4",
-"20240423-115245_Process-2",
-"20240423-065557_Process-5",
-"20240423-065549_Process-3",
-"20240423-065313_Process-4",
-"20240423-065303_Process-2",
-"20240423-015531_Process-5",
-"20240423-015528_Process-3",
-"20240423-015427_Process-4",
-"20240423-015425_Process-2",
-"20240422-205546_Process-5",
-"20240422-205546_Process-3",
-"20240422-205546_Process-2",]
+    run_names = [  # "20240422-205546_Process-4",
+        "20240423-165651_Process-3",
+        "20240423-165649_Process-5",
+        "20240423-165127_Process-4",
+        "20240423-165122_Process-2",
+        "20240423-115702_Process-5",
+        "20240423-115653_Process-3",
+        "20240423-115255_Process-4",
+        "20240423-115245_Process-2",
+        "20240423-065557_Process-5",
+        "20240423-065549_Process-3",
+        "20240423-065313_Process-4",
+        "20240423-065303_Process-2",
+        "20240423-015531_Process-5",
+        "20240423-015528_Process-3",
+        "20240423-015427_Process-4",
+        "20240423-015425_Process-2",
+        "20240422-205546_Process-5",
+        "20240422-205546_Process-3",
+        "20240422-205546_Process-2", ]
 
-    run_names = [None]
+    run_names = [
+        "20240508-171628_Process-2",
+        "20240508-171628_Process-3",
+        "20240509-043606_Process-2",
+        "20240509-043802_Process-3",
+        "20240509-164927_Process-2",
+        "20240509-164927_Process-3",
+        "20240422-205546_Process-3",
+        "20240422-205546_Process-2"
+    ]
 
     parameter_sets = []
     dataset = HarmonyDataset(
@@ -124,12 +128,13 @@ if __name__ == '__main__':
         start_nth=0,
     )
     process = 0
-    for lr in learning_rate:
-        for l1 in l1_lambda:
-            for l2 in l2_lambda:
-                for g_lr in G_LR:
-                    for d_lr in D_LR:
-                        for run_name in run_names:
+    for run_name in run_names:
+        for lr in learning_rate:
+            for l1 in l1_lambda:
+                for l2 in l2_lambda:
+                    for g_lr in G_LR:
+                        for d_lr in D_LR:
+
                             parameter_sets.append({
                                 'Process': process,
                                 'LEARNING_RATE': lr,
@@ -150,20 +155,17 @@ if __name__ == '__main__':
 
     # Remove the first 4 parameter sets
     print(f"Parameter sets: {len(parameter_sets)}")
-    parameter_sets = parameter_sets[4:]
 
     n_cuda = torch.cuda.device_count()
     n_workers = n_cuda * 2
     n_parameter_sets = len(parameter_sets)
 
-        # Setup multiprocessing
+    # Setup multiprocessing
 
     manager = Manager()
 
     shared_param_sets = manager.list(parameter_sets)
     lock = manager.Lock()
-
-
 
     processes = []
     for i in range(n_workers):
@@ -174,6 +176,3 @@ if __name__ == '__main__':
 
     for p in processes:
         p.join()
-
-
-
