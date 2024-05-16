4c1b8033d56a680411ad28ee1e3e14edc0e7595e
Erik Hamberg <erikhamb@kth.se>
2024-05-16 22:46:52 +0200

    Created a quality measure for the entire mip image

--- full_sample_measure.py
+++ full_sample_measure.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
     print("Loading dataset...")
 
     disallowed_datasets = [
-        '2307130102__2023-07-23T08_50_42-Measurement 11',
+        #'2307130102__2023-07-23T08_50_42-Measurement 11',
         #'2307130202__2023-07-23T10_32_16-Measurement 11',
         #'2307130302__2023-07-23T12_13_49-Measurement 11'
     ]
